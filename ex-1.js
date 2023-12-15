4c3ee284b3f5740d5a2a07e8c4e444258a28c4ac
bodin <bodinsonsawan@gmail.com>
2023-12-15 15:44:25 +0700

    fix(ex1-3):bodin's assignment

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,40 @@
-class EmailNotification { }
+class EmailNotification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send(email) {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
 
-class SMSNotification { }
+class SMSNotification {
+  constructor(notificationId, createdTime, content, phoneNumber) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.phoneNumber = phoneNumber;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.phoneNumber}`);
+  }
+}
+
+let emailNotificationDemo = new EmailNotification(
+  "ID:1",
+  "5:00 PM",
+  "Hi, contact from E-mail",
+  "test1@gmail.com"
+);
+
+let smsNotificationDemo = new SMSNotification(
+  "ID:2",
+  "8:00 AM",
+  "Hi, contact from SMS",
+  "123-123-123"
+);
+
+emailNotificationDemo.send();
+smsNotificationDemo.send();
--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,44 @@
-class Notification { }
+class Notification {
+  constructor(notificationId, createdTime, content) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+  }
+  sendEmail() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+  sendSms() {
+    console.log(`Notification has been sent to ${this.phoneNumber}`);
+  }
+}
 
-class EmailNotification { }
+class EmailNotification extends Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    super(notificationId, createdTime, content);
+    this.receiver = receiver;
+  }
+}
 
-class SMSNotification { }
+class SMSNotification extends Notification {
+  constructor(notificationId, createdTime, content, phoneNumber) {
+    super(notificationId, createdTime, content);
+    this.phoneNumber = phoneNumber;
+  }
+}
+
+let emailNotificationDemo = new EmailNotification(
+  "ID:1",
+  "5:00 PM",
+  "Hi, contact from E-mail",
+  "test1@gmail.com"
+);
+
+let smsNotificationDemo = new SMSNotification(
+  "ID:2",
+  "8:00 AM",
+  "Hi, contact from SMS",
+  "123-123-123"
+);
+
+emailNotificationDemo.sendEmail();
+smsNotificationDemo.sendSms();
