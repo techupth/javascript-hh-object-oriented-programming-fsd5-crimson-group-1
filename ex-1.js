6bbea3f92872a2aa654f1fb11e716e5e2ab724e5
Waramet Hatjantuek <waramet.fl@gmail.com>
2023-12-15 15:39:51 +0700

    add completed exercise

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,40 @@
-class EmailNotification { }
+class EmailNotification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  sendEmail() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
 
-class SMSNotification { }
+class SMSNotification {
+  constructor(notificationId, createdTime, content, phoneNumber) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.phoneNumber = phoneNumber;
+  }
+  sendSms() {
+    console.log(`Notification has been sent to ${this.phoneNumber}`);
+  }
+}
+
+const emailTsunamiAlert = new EmailNotification(`Alert`, 11.0, `Tsunami`, [
+  `Biden`,
+  `Putin`,
+  `Prayuth`,
+]);
+
+const smsTsunamiAlert = new SMSNotification(
+  `Alert`,
+  `11:00`,
+  `Tsunami`,
+  `099 999 9999`
+);
+
+emailTsunamiAlert.sendEmail();
+
+smsTsunamiAlert.sendSms();
--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,39 @@
-class Notification { }
+class Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
+class EmailNotification extends Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    super(notificationId, createdTime, content, receiver);
+  }
+}
 
-class EmailNotification { }
+class SMSNotification extends Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    super(notificationId, createdTime, content, receiver);
+  }
+}
 
-class SMSNotification { }
+const thailandAlertEmail = new EmailNotification(
+  `Alet1`,
+  `11.00`,
+  `Earthquake`,
+  `People of Thailand`
+);
+
+const thailandAlertSms = new SMSNotification(
+  `Alet1`,
+  `11.00`,
+  `Earthquake`,
+  `Phonenumbers of Thailand`
+);
+
+thailandAlertEmail.send();
+thailandAlertSms.send();
